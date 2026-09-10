9e5f4cc15e40aab8d20aa8aafc93eb173e5d79e0
namyagaur <namyaagaur@gmail.com>
2026-09-10 12:18:21 +0530

    done

--- main.py
+++ main.py
@@ -1,29 +1,51 @@
 from pathlib import Path
+folders = Path("documents")
+def load_documents(folders):
+    files = folders.glob("*.txt")
+    documents = []
+    for f in files:
+        documents.append({
+            "source" : f.name,
+            "text": f.read_text(),
+            "tokens": tokenize(f.read_text())
+        })
 
-folders = Path('documents')
-files = folders.glob('*.txt')
+    return documents
 
-documents = []
-for file in files:
-    documents.append({
-        "source":file.name,
-        "text": file.read_text()
-
-    })
+def normalize_text(text):
+    return text.strip().lower()
 
 def tokenize(text):
-    res = text.strip()
-    text = res.lower()
-    return text.split()
-
-
-for d in documents:
-    d["tokens"] = tokenize(d["text"])
+    return normalize_text(text).split()
 
 def process_query(query):
-    return query.strip().lower().split()
+    return tokenize(query)
 
-query = input("Enter your query")
+def doc_token(docs):
+    for d in docs:
+        d['tokens'] = tokenize(d['text'])
 
-print(process_query(query))
+def calculate_score(query,doc_token):
+    query_token = process_query(query)
+    score = 0
+    for q in query_token:
+        if q in doc_token:
+            score += 1
+    return score
 
+def rank_res(res):
+    res.sort(key = lambda x : x['score'], reverse = True)
+def retrieve(query,docs,k):
+    res = []
+    for d in docs:
+
+        res.append({
+            'source': d['source'],
+            'score' : calculate_score(query,d['tokens'])
+        })
+
+    rank_res(res)
+    return res[:k]
+
+def display_res(res):
+    print(res)
